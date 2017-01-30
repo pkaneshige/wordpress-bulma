20487b45895ea70d9c2decde9ad8f7a7f86d0e98
Pace Kaneshige <pace@pasteandtape.com>
2017-01-30 17:12:16 -0500

    Added font-awesome to load_css.

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
   function load_css() {
     wp_enqueue_style( 'bulma_stylesheet', '//cdnjs.cloudflare.com/ajax/libs/bulma/0.3.1/css/bulma.css' );
     wp_enqueue_style( 'bulma_stylesheet_map', '//cdnjs.cloudflare.com/ajax/libs/bulma/0.3.1/css/bulma.css.map' );
+    wp_enqueue_style( 'font_awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css');
   }
 
   add_action( 'wp_enqueue_scripts', 'load_css' );
